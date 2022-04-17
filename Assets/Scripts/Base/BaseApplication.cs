b8f70eb893fbd824914a78603a172563279645ae
chsbin99 <chsbin99@gmail.com>
2022-04-17 21:16:36 +0900

    +InGameController , IngameModel, IngameView, IngameApplication, BaseApplication, GameManager
    baseapplication 추가
    ingame amvc 추가함
    ingame state : Loading(상대 대기), Battle(몬스터라운드), UpGrade(업그레이드시간), Pause(상대방이 끊기거나 이상이 생긴경우)로 나눔, 세부 개발 안함

---
+++ Assets/Scripts/Base/BaseApplication.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseElement : MonoBehaviour
+{
+    public BaseApplication app
+    {
+        get
+        {
+            return GameObject.FindObjectOfType<BaseApplication>();
+        }
+    }
+    public interface IBaseController
+    {
+        void Init();
+        void Set();
+        void AdvanceTime(float dt_sec);
+        void Dispose();
+        void SetActive(bool flag);
+    }
+}
+public abstract class BaseApplication : MonoBehaviour
+{
+    public void SetActive(bool flag)
+    {
+        this.gameObject.SetActive(flag);
+    }
+    public abstract void Init();
+    public abstract void AdvanceTime(float dt_sec);
+    public abstract void Set();
+    public abstract void Dispose();
+}
---
+++ Assets/Scripts/InGame/InGameController.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameController : BaseElement, BaseElement.IBaseController
+{
+    private InGameApplication _app = new InGameApplication();
+    public void Init()
+    {
+        _app = app as InGameApplication;
+        InitHandlers();
+        ChangeState(EInGameState.LOADING);
+    }
+
+    public void Set()
+    {
+
+    }
+
+    public void AdvanceTime(float dt_sec)
+    {
+        if (_currentState != EInGameState.UNKNOWN)
+        {
+            GetStateHandler(_currentState).AdvanceTime(Time.deltaTime);
+        }
+    }
+
+    public void Dispose()
+    {
+        if (_currentState != EInGameState.UNKNOWN)
+        {
+            GetStateHandler(_currentState).Dispose();
+        }
+    }
+
+    public void SetActive(bool flag)
+    {
+
+    }
+
+
+    #region State Handlers Base
+    private Dictionary<EInGameState, IInGameStateHandler> _handlers = new Dictionary<EInGameState, IInGameStateHandler>();
+    private EInGameState _currentState = EInGameState.UNKNOWN;
+
+    private void InitHandlers()
+    {
+        _handlers.Clear();
+        _handlers.Add(EInGameState.LOADING, new StateHandlerLoading());
+        _handlers.Add(EInGameState.BATTLE, new StateHandlerBattle());
+        _handlers.Add(EInGameState.UPGRADE, new StateHandlerUpgrade());
+        _handlers.Add(EInGameState.PAUSE, new StateHandlerPause());
+
+        foreach (EInGameState state in _handlers.Keys)
+        {
+            _handlers[state].Init();
+        }
+    }
+
+
+    private void ChangeState(EInGameState nextState)
+    {
+        if (nextState != EInGameState.UNKNOWN && nextState != _currentState)
+        {
+            EInGameState prevState = _currentState;
+            _currentState = nextState;
+            IInGameStateHandler leaveHandler = GetStateHandler(prevState);
+            if (leaveHandler != null)
+            {
+                leaveHandler.Dispose();
+            }
+            IInGameStateHandler enterHandler = GetStateHandler(_currentState);
+            if (enterHandler != null)
+            {
+                enterHandler.Set();
+            }
+        }
+    }
+
+    private IInGameStateHandler GetStateHandler(EInGameState state)
+    {
+        if (_handlers.ContainsKey(state))
+        {
+            return _handlers[state];
+        }
+        return null;
+    }
+    #endregion
+
+    #region State Handler Class
+    protected class StateHandlerLoading : IInGameStateHandler
+    {
+        public void Init()
+        {
+        }
+
+        public void Set()
+        {
+        }
+
+        public void AdvanceTime(float dt_sec)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+
+    protected class StateHandlerBattle : IInGameStateHandler
+    {
+        public void Init()
+        {
+        }
+
+        public void Set()
+        {
+        }
+
+        public void AdvanceTime(float dt_sec)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+
+    protected class StateHandlerUpgrade : IInGameStateHandler
+    {
+        public void Init()
+        {
+        }
+
+        public void Set()
+        {
+        }
+
+        public void AdvanceTime(float dt_sec)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+
+    protected class StateHandlerPause : IInGameStateHandler
+    {
+        public void Init()
+        {
+        }
+
+        public void Set()
+        {
+        }
+
+        public void AdvanceTime(float dt_sec)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+    }
+    #endregion
+}
+public interface IInGameStateHandler
+{
+    void Init();
+    void Set();
+    void AdvanceTime(float dt_sec);
+    void Dispose();
+}
+
+public enum EInGameState
+{
+    UNKNOWN,
+    LOADING,
+    BATTLE,
+    UPGRADE,
+    PAUSE,
+}
