9dec4a5952fa4d8e3d94791857b05909a78795bf
chsbin99 <chsbin99@gmail.com>
2022-05-11 10:07:36 +0900

    +add lobbyScene ui

--- Assets/Scripts/Lobby/LobbyUI.cs
+++ Assets/Scripts/Lobby/LobbyUI.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyUI : MonoBehaviour
 {
+    [SerializeField]
+    private Text _txtTopBarName;
+    public void Start()
+    {
+        _txtTopBarName.text = "USERNAME : "+NetworkManager.Instance.UserName;
+    }
     public void OnClick_CreateRoom()
     {
         NetworkManager.Instance.CreateRoom();
